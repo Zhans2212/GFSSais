79d6571d7c7d15c8f393ec99a367f19a377e5ca3
Zhans2212 <turzhanova.zhan@gmail.com>
2026-02-19 15:09:41 +0500

    Reports button functionality implemented

--- app/static/js/base.js
+++ app/static/js/base.js
@@ -1,4 +1,20 @@
 document.addEventListener("DOMContentLoaded", () => {
+    // Кнопка отчетов
+    const reportsBtn = document.getElementById("reports-button");
+    if (reportsBtn) {
+        reportsBtn.addEventListener("click", async (e) => {
+            e.preventDefault();
+
+            const response = await fetch("/reports", {
+                method: "GET"
+            });
+            if (response.ok) {
+                window.location.href = "/reports";
+            } else {
+                alert("Ошибка!");
+            }
+        });
+    }
 
     // Кнопка профиля
     const profileBtn = document.getElementById("profile-button");
