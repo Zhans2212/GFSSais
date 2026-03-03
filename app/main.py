ba5b37e038f11839e53fa21b0f9b109e6da3b413
Zhans2212 <turzhanova.zhan@gmail.com>
2026-03-03 14:49:36 +0500

    redirect from "/"

--- app/main.py
+++ app/main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from fastapi.responses import HTMLResponse, RedirectResponse
 
 from app.config import setup_static
 from app.routers import reports
@@ -12,3 +13,7 @@ setup_static(app)
 app.include_router(reports.router, prefix="/reports", tags=["Reports"])
 app.include_router(auth.router, prefix="/login", tags=["Login"])
 app.include_router(user.router, prefix="/profile", tags=["User"])
+
+@app.get("/", response_class=HTMLResponse)
+async def home():
+    return RedirectResponse("/reports", status_code=303)
