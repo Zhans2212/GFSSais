2d4a6ad6b75fa3a7c50131a7af055b1dd9339e2c
Zhans2212 <turzhanova.zhan@gmail.com>
2026-04-20 15:40:15 +0500

    fixed approve all

--- app/db/update_tables.py
+++ app/db/update_tables.py
@@ -6,29 +6,41 @@ from app.db.engine import engine
 def bulk_accept_all(typ: str, post: str, fio: str, package_name: str = "DASORP_TEST") -> None:
     empt = None
 
-    if typ == "all":
-        code = empt
-        type_payer = empt
-    elif typ == "sz":
-        code = empt
-        type_payer = "СЗ"
-    elif typ == "so":
-        code = "СО"
-        type_payer = empt
-    elif typ == "ep":
-        code = "ЕП"
-        type_payer = empt
+    match typ:
+        case "all":
+            code = empt
+            type_payer = empt
+        case "sz":
+            code = empt
+            type_payer = "СЗ"
+        case "so":
+            code = "СО"
+            type_payer = empt
+        case "ep":
+            code = "ЕП"
+            type_payer = empt
+        case _:
+            raise HTTPException(status_code=400, detail="Invalid type")
+
+    if code is None and type_payer is None:
+        query = text(
+            f"BEGIN {package_name}.MANAGE.APPROVE_ALL(:post, :fio); END;"
+        )
+
+        with engine.begin() as conn:
+            conn.execute(query, {
+                "post": post,
+                "fio": fio
+            })
     else:
-        raise HTTPException(status_code=400, detail="Invalid type")
+        query = text(
+            f"BEGIN {package_name}.MANAGE.APPROVE_ALL(:code, :type_payer, :post, :fio); END;"
+        )
 
-    query = text(
-        f"BEGIN {package_name}.MANAGE.APPROVE_ALL(:code, :type_payer, :post, :fio); END;"
-    )
-
-    with engine.begin() as conn:
-        conn.execute(query, {
-            "code": code,
-            "type_payer": type_payer,
-            "post": post,
-            "fio": fio
-        })
+        with engine.begin() as conn:
+            conn.execute(query, {
+                "code": code,
+                "type_payer": type_payer,
+                "post": post,
+                "fio": fio
+            })
